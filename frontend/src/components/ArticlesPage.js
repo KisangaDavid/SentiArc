e613e9403eaefa6c7346917dee6da2b3e3fe2414
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-07-12 08:42:12 -0500

    added routes for more pages and put components in their own folder

---
+++ frontend/src/components/ArticlesPage.js
@@ -0,0 +1,11 @@
+function ArticlesPage() {
+    return (
+        <div className="articlesPage">
+          <header className="App-header">
+          <h1>This is the articles page!</h1>
+          </header>
+        </div>
+    );
+  }
+
+  export default ArticlesPage;
