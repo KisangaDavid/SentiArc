647ae7e5b9a0f38b825f611f10f801ed02dd2e8b
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-07-12 16:30:48 -0500

    updated packages, created Navbar, switched linking to backend to Navbar component from InputAndSubmit component

--- frontend/src/components/PostSubmitPage.js
+++ frontend/src/components/PostSubmitPage.js
@@ -1,16 +1,62 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import {Navbar, Nav} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+import axios from 'axios';
+import AppNavbar from './AppNavbar';
+import React, { useState, useEffect } from 'react';
+
+
+
 function PostSubmitPage(props) {
+  const [data, setData] = useState([]);
   const location = useLocation();
   const companyName = location.state.companyName;
-
-  console.log(props);
+  useEffect(() => {
+    console.log("I fire once");
+    getData(companyName);
+  }, []);
+  
     return (
         <div className="PostSubmitPage">
+      {/* Pass prop (companyName) here */}
+          <AppNavbar companyName={companyName}/>
           <header className="App-header">
           <h1>This is the Post Submit page! Company Submitted: {companyName} </h1>
           </header>
         </div>
     );
   }
+
+function getData(input) {
+    axios({
+        method: "GET",
+        url:"/article/",
+        params: {
+            companyName: input
+        }
+      }).then((response)=>{
+        const data = response.data
+        const convertedData = convertData(data)
+        console.log(convertedData[0].title)
+        console.log(convertedData[0].author)
+        console.log(convertedData[0].time)
+        console.log(convertedData[0].completeLink)
+        console.log(convertedData[0].imgSource)       
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          }
+      })}
+
+function convertData(data) {
+    var parsed = JSON.parse(data);
+    return parsed
+    
+}
+  
+
+
 
   export default PostSubmitPage;
