1e93b6001eb005324590ac0de1c5fdaecc12e9cf
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-07-12 09:52:22 -0500

    button on home page redirects to PostSubmitPage and passes the company inputted as state

--- frontend/src/components/PostSubmitPage.js
+++ frontend/src/components/PostSubmitPage.js
@@ -1,8 +1,13 @@
-function PostSubmitPage() {
+import { useLocation } from "react-router-dom"
+function PostSubmitPage(props) {
+  const location = useLocation();
+  const companyName = location.state.companyName;
+
+  console.log(props);
     return (
         <div className="PostSubmitPage">
           <header className="App-header">
-          <h1>This is the Post Submit page!</h1>
+          <h1>This is the Post Submit page! Company Submitted: {companyName} </h1>
           </header>
         </div>
     );
