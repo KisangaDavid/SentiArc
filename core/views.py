0efc9e39c28f3acef08ac719e578cd709439ba6d
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-07-13 15:30:09 -0500

    created seperate folder for backend scripts

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 # from rest_framework import status
 # from .serializers import NewsArticleSerializer
 # from .models import NewsArticle
-from .customGoogleNewsAPI import GetNewsLink
+from .scripts.customGoogleNewsAPI import GetNewsLink
 
     
 def front(request):
