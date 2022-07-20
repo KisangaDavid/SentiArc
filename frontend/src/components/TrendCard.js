e434a805b286ad9c5efd1b8db0693b9ffe31d0af
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-07-20 10:49:55 -0500

    fixed TrendCard

--- frontend/src/components/TrendCard.js
+++ frontend/src/components/TrendCard.js
@@ -12,7 +12,7 @@ class TrendCard extends React.Component {
             trendStat: props.trendStat,
         }
     }
-//test
+
     render() {
         return (
             <Card className="trendsCard" >
